1fd8a9b2294ab31d3958a7f074bb4fb340a26748
mikenandi <aidlero@yahoo.com>
2022-06-16 20:45:16 +0300

    fixing bug on catching wrong password error

--- api/controllers/auth/login.js
+++ api/controllers/auth/login.js
@@ -1,7 +1,7 @@
-// --module imports.
-const path = require("path"); // ::FOR getting path of the file.
-const fs = require("fs"); // ::For entering OS FILE SYSTEM.
-const jsonwebtoken = require("jsonwebtoken"); // ::For creating token.
+// Module imports.
+const path = require("path"); // 👉 FOR getting path of the file.
+const fs = require("fs"); // 👉 For entering OS FILE SYSTEM.
+const jsonwebtoken = require("jsonwebtoken"); // 👉 For creating token.
 
 module.exports = {
   friendlyName: "Login",
@@ -40,10 +40,10 @@ module.exports = {
 
   fn: async function (inputs, exits) {
     try {
-      // --finding the user in our database.
+      // Finding the user in our database.
       let user = await User.findOne({ where: { email: inputs.email } });
 
-      // --when the user is not found in the database.
+      // When the user is not found in the database.
       if (!user)
         return exits.failure({
           success: false,
@@ -51,35 +51,29 @@ module.exports = {
           message: "username was not found.",
         });
 
-      // --comparing passwords btn entered password password from database.
-      await sails.helpers.passwords
-        .checkPassword(inputs.password, user.password)
-        .intercept("incorrect", (err) => {
-          // --response when user entered wrong password.
-          return exits.passwordMismatch({
-            success: false,
-            code: "wrong_password",
-            message: "wrong password",
-          });
-        });
+      // Comparing passwords btn entered password password from database.
+      await sails.helpers.passwords.checkPassword(
+        inputs.password,
+        user.password
+      );
 
-      // --getting private key.
+      // Getting private key.
       const pathToKey = path.resolve("secrets/priv_key.pem");
       const PRIV_KEY = fs.readFileSync(pathToKey, "utf8");
 
-      // --creating payload.
+      // Creating payload.
       let payload = {
         sub: user.id,
         iat: Date.now(),
       };
 
-      // --signing new token to user.
+      // Signing new token to user.
       let signedToken = jsonwebtoken.sign(payload, PRIV_KEY, {
         algorithm: "RS256",
         expiresIn: "1d",
       });
 
-      // --response for succesfull signup.
+      // Response for succesfull signup.
       return exits.success({
         success: true,
         message: "successfull login",
@@ -90,7 +84,16 @@ module.exports = {
         },
       });
     } catch (error) {
-      // --any other failure on login.
+      // catching when user entered a wrong password
+      if (error.code === "incorrect") {
+        return exits.passwordMismatch({
+          success: false,
+          code: "wrong_password",
+          message: "wrong password",
+        });
+      }
+
+      // catching any other failure on login.
       return exits.failure({
         success: false,
         code: error.code,
