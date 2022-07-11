3f371812927bf636a3c6f304a75971392afe27a6
mikenandi <aidlero@yahoo.com>
2022-07-11 06:33:11 +0300

    adding user profile route

---
+++ api/controllers/user/profile.js
@@ -0,0 +1,42 @@
+module.exports = {
+  friendlyName: "Profile",
+
+  description: "Profile of user.",
+
+  inputs: {
+    userId: {
+      type: "string",
+      required: true,
+    },
+  },
+
+  exits: {
+    success: {
+      statusCode: 200,
+      description: "successful query",
+    },
+    failure: {
+      statusCode: 400,
+      description: "failed query",
+    },
+  },
+
+  fn: async function (inputs, exits) {
+    try {
+      let userRecord = await User.findOne({ where: { id: inputs.userId } });
+
+      return exits.success({
+        success: true,
+        message: "successful response",
+        data: userRecord,
+      });
+    } catch (error) {
+      // catching any other error.
+      return exits.failure({
+        success: false,
+        code: error.code,
+        message: error.message,
+      });
+    }
+  },
+};
--- config/routes.js
+++ config/routes.js
@@ -23,4 +23,5 @@ module.exports.routes = {
   "GET /api/v1/task/tasks": { action: "task/tasks" },
   "POST /api/v1/event/create": { action: "event/create" },
   "GET /api/v1/event/events": { action: "event/events" },
+  "GET /api/v1/user/profile": { action: "user/profile" },
 };
