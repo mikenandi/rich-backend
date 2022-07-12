f17d18d08c3e5a6e3d4e3fb76c487b3ddf21bdd7
mikenandi <aidlero@yahoo.com>
2022-07-12 04:49:47 +0300

    adding sending email route

---
+++ api/helpers/send-email.js
@@ -0,0 +1,68 @@
+// --Imports of userful modules of sending email.
+const nodemailer = require("nodemailer");
+const mailgun = require("nodemailer-mailgun-transport");
+
+module.exports = {
+  friendlyName: "Send email",
+
+  description:
+    "Helper function for sending email using mailgun and nodemailer. ",
+
+  inputs: {
+    sendTo: {
+      type: "string",
+      isEmail: true,
+      required: true,
+      description: "Email of the person receiving recovery link.",
+    },
+    subject: {
+      type: "string",
+      required: true,
+      description: "Subject of email. ",
+    },
+    text: {
+      type: "string",
+      required: true,
+      description: "Message that needs to be delivered of email. ",
+    },
+  },
+
+  exits: {
+    failure: {
+      description: "Error occured. ",
+    },
+    success: {
+      description: "Email was sent.",
+    },
+  },
+
+  fn: async function (inputs, exits) {
+    // --Authentication details.
+    let mailgunAuth = {
+      auth: {
+        api_key: sails.config.custom.mailgunApiKey,
+        domain: sails.config.custom.mailgunDomain,
+      },
+    };
+
+    let nodemailerMailgun = nodemailer.createTransport(mailgun(mailgunAuth));
+
+    // --Sending email.
+    nodemailerMailgun.sendMail(
+      {
+        from: `<Evento ${sails.config.custom.fromEmail}>`,
+        headers: sails.config.custom.fromName,
+        to: inputs.sendTo,
+        subject: inputs.subject,
+        text: inputs.text,
+      },
+      (error, response) => {
+        // --Checking if there was error during sending email.
+        if (error) return exits.failure();
+
+        // --Response for successfull email sent
+        return exits.success();
+      }
+    );
+  },
+};
--- config/custom.js
+++ config/custom.js
@@ -17,8 +17,8 @@ module.exports.custom = {
   // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
   // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
   // …
-  accountSid: "ACdaba4e0ae159d2b917caa6b71ce31973",
-  authToken: "66b2e0d3987fb327e43a3e2c9cdbc1be",
-  fromNumber: "+19302069573",
-  toNumber: "+255**********",
+  mailgunApiKey: "634f082fbfa0ca5054fc5491c272a62f-53ce4923-5d1312be",
+  domain: "https://api.eu.mailgun.net/v3/gudsurvey.ml/messages",
+  fromEmail: "gudsurvey@gmail.com",
+  fromName: "Richard Technologies.",
 };
